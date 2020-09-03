5e41e2376f838e7f561b725da8fd2335933e14cc
axelhellberg <axhe1901@student.miun.se>
2020-09-03 15:52:04 +0200

    added files

---
+++ js/main.js
@@ -0,0 +1,16 @@
+var toggleSwitch = function (e) {
+    if (!e.target.classList.contains('toggle')) return;       
+    e.target.classList.toggle('on');
+    document.querySelector('body').classList.toggle('on');
+};
+
+document.addEventListener('click', toggleSwitch, false);
+
+document.addEventListener('mousedown', function (e) {
+    if (!e.target.classList.contains('toggle')) return;
+    e.preventDefault();
+}, false);
+
+document.addEventListener('keypress', function (e) {
+    if (e.key === 'Enter' || e.key === ' ') toggleSwitch(e);
+}, false);
